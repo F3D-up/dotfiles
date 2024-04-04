e86c73f9e3f74008c147c4327be19ce778e804fe
F3D-up <80313567+F3D-up@users.noreply.github.com>
2024-04-04 14:57:33 +0200

    Neovim configuration continuation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,17 +8,22 @@ vim.cmd("set autoindent")
 vim.cmd("set relativenumber")
 vim.cmd("set smarttab")
 vim.cmd("set number")
+vim.api.nvim_set_keymap("n", "<C-k>", ":wincmd k<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", ":wincmd j<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-h>", ":wincmd h<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>", ":wincmd l<CR>", { silent = true })
+vim.api.nvim_set_keymap('n', ',m', ':%s/\\r//g<CR>', { noremap = true, silent = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -27,6 +32,6 @@ local opts = {}
 require("lazy").setup("plugins")
 vim.api.nvim_create_augroup("cursorfix", { clear = true })
 vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
-  command = "set guicursor=a:ver1-blinkon1",
-  group = "cursorfix",
+	command = "set guicursor=a:ver1-blinkon1",
+	group = "cursorfix",
 })
